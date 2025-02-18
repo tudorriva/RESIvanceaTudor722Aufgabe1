29fce11a3c9f4b69a021d7203fbd233eb1e0b1f1
tudorriva <tudorivanceastefan@gmail.com>
2025-02-18 11:02:32 +0200

    Added Ubung 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +18,19 @@ public class Main {
         }
 
         // Ubung 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kraftpunkte: ");
+        double kraftpunkte = scanner.nextDouble();
+        Set<String> uniqueNames = new HashSet<>();
+        for(Schlachten schlachtObj : schlachten) {
+            String ninjaName = schlachtObj.getCharakterName();
+            if (schlachtObj.getKraftpunkte() > kraftpunkte) {
+                uniqueNames.add(schlachtObj.getCharakterName());
+            }
+        }
+        uniqueNames.forEach(System.out::println);
+
+        // Ubung 3
+
     }
 }
